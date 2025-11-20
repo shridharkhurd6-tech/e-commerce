b8b8686e8ec59d167de8b15880f804271239c926
T-A17 <truptiambekar17@gmail.com>
2025-11-20 18:31:05 +0530

    message

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./AddProduct.css";
 //import Login from "../../Pages/Login/Login";
 //import EditProductmodal from "../EditProductModal";
